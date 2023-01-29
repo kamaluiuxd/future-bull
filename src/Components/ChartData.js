5bde0081346b1f18fe3a7d570f95f35d7afb629b
kamal <kamaluiuxd@gmail.com>
2023-01-29 19:48:03 +0530

    updated Chart

--- src/Components/ChartData.js
+++ src/Components/ChartData.js
@@ -20,7 +20,8 @@ const options = {
 
 const ChartData = ({ tradeData }) => {
 	console.log(tradeData);
-	const labels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+	const labels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+	// const labels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 	const data = {
 		labels,
@@ -28,15 +29,19 @@ const ChartData = ({ tradeData }) => {
 			{
 				label: "Net Call OI",
 				// data: labels.companyOpenIndex.map((lb) => lb.intraDayCallNet),
-				data: [12, 15, 13, 18, 11, 19, 16, 14, 12, 18, 17, 15],
+				data: [12, 15, 13, 18, 11, 19, 16, 14, 12, 18, 17, 15, 12, 15, 13, 18, 11, 19, 16, 14, 12, 18, 17, 15, 16, 14, 12, 18, 17, 15],
 				backgroundColor: "green",
 			},
-			{ label: "Put Call OI", data: [8, 5, 7, 2, 9, 1, 4, 8, 2, 3, 5, 5], backgroundColor: "red" },
+			{
+				label: "Put Call OI",
+				data: [8, 5, 7, 2, 9, 1, 4, 8, 2, 3, 5, 5, 8, 5, 7, 2, 9, 1, 4, 8, 2, 3, 5, 5, 4, 8, 2, 3, 5, 5],
+				backgroundColor: "red",
+			},
 		],
 	};
 	console.log(data);
 	return (
-		<div className="h-[300px]">
+		<div className="h-[250px]">
 			<Bar options={options} data={data} />
 		</div>
 	);
