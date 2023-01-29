7f82fc435f8b146a6209a8715223a8fdedbb2368
kamal <kamaluiuxd@gmail.com>
2023-01-29 12:36:04 +0530

    Date Picker Cat Menu Set

---
+++ src/Components/CatMenu.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+const current = new Date();
+const currentDate = `${current.getDate()}/${current.getMonth() + 1}/${current.getFullYear()}`;
+
+const CatMenu = ({ tradeData, setTradeData }) => {
+	const [date, setDate] = useState(currentDate);
+
+	console.log(date);
+
+	const filterItem = (company) => {
+		const updatedItem = tradeData.filter((currentElement) => {
+			return currentElement.companyName === company;
+		});
+		setTradeData(updatedItem);
+	};
+	return (
+		<>
+			<div className="flex justify-center container">
+				<div className=" my-5 flex justify-center">
+					<div className="flex justify-evenly space-x-4 text-white">
+						<button className="bg-black px-5 py-2 rounded-md" onClick={() => filterItem("Client")}>
+							Client
+						</button>
+						<button className="bg-black px-5 py-2 rounded-md" onClick={() => filterItem("DII")}>
+							DII
+						</button>
+						<button className="bg-black px-5 py-2 rounded-md" onClick={() => filterItem("FII")}>
+							FII
+						</button>
+						<button className="bg-black px-5 py-2 rounded-md" onClick={() => filterItem("Pro")}>
+							Pro
+						</button>
+					</div>
+				</div>
+			</div>
+			<div className="space-y-5">
+				<h1 className="text-2xl font-bold">Expiry</h1>
+				<input className="border border-black p-2" type="date" value={date} onChange={(e) => setDate(e.target.value)} />
+				<h1 className="text-2xl font-bold">{date}</h1>
+			</div>
+		</>
+	);
+};
+export default CatMenu;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,20 +1,32 @@
 import { useEffect, useState } from "react";
-import { default as TableData, default as TradeCard } from "../Components/TradeCard";
+import CatMenu from "../Components/CatMenu";
+import TradeCard from "../Components/TradeCard";
 
 import data from "../data.json";
 
 const Home = () => {
 	const [tradeData, setTradeData] = useState([]);
-
 	useEffect(() => {
 		setTradeData(data.openIndex);
 	}, []);
 
+	const filterItem = (company) => {
+		// const updatedItem = tradeData.companyOpenIndex.filter((currentElement) => {
+		// 	return currentElement.companyName === company;
+		// });
+		// setTradeData(updatedItem);
+		// console.log(updatedItem);
+	};
+
+	console.log(tradeData);
 	return (
 		<section className="w-[1400px] mx-auto">
+			<CatMenu />
 			<section className="m-10">
-				<TradeCard tradeData={tradeData} />
-				<TableData tradeData={tradeData} />
+				<section className="grid grid-cols-2">
+					<TradeCard tradeData={tradeData} />
+					<TradeCard tradeData={tradeData} />
+				</section>
 			</section>
 		</section>
 	);
